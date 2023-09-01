2f0dd8952c73d737c6cfa42c7a3eedad12e97609
Zool(q) <mr.felix.scholz@web.de>
2023-09-01 14:30:55 +0200

    Added exponent constants

--- memu/src/constants.rs
+++ memu/src/constants.rs
@@ -5,23 +5,44 @@ pub const BITS: u64 = 8;
 /// This is not recomended you use it.
 pub const BYTE: u64 = 1;
 
+/// Exponent of 2 for a kilo byte.
+pub const KILOBYTE_EXP: u8 = 10;
+
 /// Amount of bytes in a kilo byte.
 pub const KILOBYTE: u64 = 1024;
+
+/// Exponent of 2 for a mega byte.
+pub const MEGABYTE_EXP: u8 = KILOBYTE_EXP + 10;
 
 /// Amount of bytes in a mega byte.
 pub const MEGABYTE: u64 = KILOBYTE * 1024;
 
+/// Exponent of 2 for a giga byte.
+pub const GIGABYTE_EXP: u8 = MEGABYTE_EXP + 10;
+
 /// Amount of bytes in a giga byte.
 pub const GIGABYTE: u64 = MEGABYTE * 1024;
+
+/// Exponent of 2 for a tera byte.
+pub const TERABYTE_EXP: u8 = GIGABYTE_EXP + 10;
 
 /// Amount of bytes in a tera byte.
 pub const TERABYTE: u64 = GIGABYTE * 1024;
 
+/// Exponent of 2 for a tera byte.
+pub const PETABYTE_EXP: u8 = TERABYTE_EXP + 10;
+
 /// Amount of bytes in a peta byte.
 pub const PETABYTE: u64 = TERABYTE * 1024;
 
+/// Exponent of 2 for a exa byte.
+pub const EXABYTE_EXP: u8 = PETABYTE_EXP + 10;
+
 /// Amount of bytes in a exa byte.
 pub const EXABYTE: u64 = PETABYTE * 1024;
+
+/// Exponent of 2 for a zetta byte.
+pub const ZETTABYTE_EXP: u8 = EXABYTE_EXP + 10;
 
 /// Amount of bytes in a zetta byte.
 pub const ZETTABYTE: u128 = EXABYTE as u128 * 1024;
